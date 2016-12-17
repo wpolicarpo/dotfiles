6acb6211de09976f1f246754df515456db74571f
Wanderson Policarpo <wpolicarpo@gmail.com>
2016-12-17 16:57:18 +0000

    Add install script

--- README.md
+++ README.md
@@ -1,21 +1,15 @@
+# How to install
+
+```
+cd ~
+git clone git@github.com:wpolicarpo/dotfiles.git ~/.dotfiles
+cd .dotfiles
+./install
+```
+
 # Dependencies
 
-- [Homebrew](http://brew.sh)
+- [Alfred](https://www.alfredapp.com)
+- [iTerm2](https://www.iterm2.com/downloads.html)
+- [Virtualbox](https://www.virtualbox.org/wiki/Downloads)
 - [Vagrant](https://www.vagrantup.com/)
-
-# Packages
-
-- ansible
-- wget
-- rbenv
-- mysql-connector-c (`mysql2` gem)
-
-
-# TODO
-
-- `wget https://packagecontrol.io/Package%20Control.sublime-package -O ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package`
-- `ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime`
-- `ln -s ~/.dotfiles/.*rc ~`
-- `ln -s ~/.dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings`
-- `ln -s ~/.dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap`
-- Load iTerm file: `~/.dotfiles/iterm/com.googlecode.iterm2.plist`
---
+++ install.sh
@@ -0,0 +1,102 @@
+#!/bin/zsh
+# Configure tools and dotfiles
+#
+# Execute this script with the command below:
+# \curl -sSL https://raw.githubusercontent.com/wpolicarpo/dotfiles/master/install.sh | bash -s
+
+set -e
+
+function pecho() {
+  print -P "%B%F{$1}${2}%f%b"
+}
+
+export DOTFILES=~/.dotfiles
+
+#######################################################################################################################
+# BEGIN: oh my zsh ####################################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring oh my zsh ###"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#######################################################################################################################
+# BEGIN: brew #########################################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring brew ###"
+
+if [ ! -x /usr/local/bin/brew ]
+then
+  pecho "green" "[+] Installing brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  pecho "yellow" "[!] brew is already installed"
+fi
+
+brew update > /dev/null
+
+#######################################################################################################################
+# BEGIN: tools ########################################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring tools ###"
+
+TOOLS=(wget node mysql-connector-c)
+
+for tool in "${TOOLS[@]}"
+do
+  if brew ls --versions ${tool} > /dev/null
+  then
+    pecho "yellow" "[!] ${tool} is already installed"
+  else
+    pecho "green" "[+] Installing ${tool}"
+    brew install $tool
+  fi
+done
+
+#######################################################################################################################
+# BEGIN: rbenv ########################################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring rbenv ###"
+
+if ! type "rbenv" > /dev/null
+then
+  pecho "green" "[+] Installing rbenv"
+  brew install rbenv
+else
+  pecho "yellow" "[!] rbenv is already installed"
+fi
+
+#######################################################################################################################
+# BEGIN: Sublime Text 3 ###############################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring Sublime Text ###"
+
+export SUBLIME_PATH=~/Library/Application\ Support/Sublime\ Text\ 3
+export SUBLIME_PACKAGES_PATH="$SUBLIME_PATH/Packages/User"
+
+if [ ! -d "$SUBLIME_PATH" ]; then
+  pecho "red" "[-] Sublime Text 3 is not installed"
+fi
+
+pecho "green" "[+] Installing Package Control"
+wget -q https://packagecontrol.io/Package%20Control.sublime-package -O $SUBLIME_PATH/Installed\ Packages/Package\ Control.sublime-package
+
+pecho "green" "[+] Installing terminal shortcut"
+ln -fs "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+
+pecho "green" "[+] Installing user preferences"
+ln -fs "${DOTFILES}/sublime/Preferences.sublime-settings" "${SUBLIME_PACKAGES_PATH}/Preferences.sublime-settings"
+
+pecho "green" "[+] Installing user key bindings"
+ln -fs "${DOTFILES}/sublime/Default\ \(OSX\).sublime-keymap" "${SUBLIME_PACKAGES_PATH}/Default\ \(OSX\).sublime-keymap"
+
+#######################################################################################################################
+# BEGIN: rc files #####################################################################################################
+#######################################################################################################################
+pecho "magenta" "### Configuring *rc files ###"
+
+for file in ${DOTFILES}/rc/*
+do
+  pecho "green" "[+] Installing $file"
+  ln -fs $file ~/.$(basename $file)
+done
+
